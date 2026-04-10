d4cc1c174b4a9d25b0926e22a59648fc7588a502
Rushivez <ruschywez@gmail.com>
2026-04-10 14:38:10 +0700

    yo bro, I guess I can leave encryption now

--- src/encryption.py
+++ src/encryption.py
@@ -7,39 +7,42 @@ import bcrypt
 
 class CryptManager:
     def __init__(self):
-        self.logger = setup_logger(__name__)  # logger creating
+        self.__logger = setup_logger(__name__)  # logger creating
         self.__fernet = None
-        self.logger.debug("Checking env for keys...")
+        self.__logger.debug("Checking env for keys...")
 
-        # Загружаем .env перед чтением переменных
+        # download .env before reading data
         env_path = Path(__file__).resolve().parent.parent / '.env'
         load_dotenv(env_path)
 
         try:  # try to load keys
             self.__key = self.__load_key()
-            self.logger.info("Keys successfully downloaded")
-        except ValueError:  # if error then create it
-            self.logger.warning("EnvironmentError")
+            self.__logger.info("Keys successfully downloaded")
+        except ValueError as e:  # if error then create it
+            self.__logger.warning(e)
             self.__key = self.__create_key(env_path)
-            self.logger.info("Keys were created successfully")
+            self.__logger.info("Keys were created successfully")
 
         self.__fernet = Fernet(self.__key)
 
     def __load_key(self) -> bytes:
         ENCRYPTION_KEY = os.getenv('ENCRYPTION_KEY')
         if ENCRYPTION_KEY is None:
-            raise ValueError
+            raise ValueError("ENCRYPTION_KEY is not found")
         ENCRYPTION_KEY = bytes.fromhex(ENCRYPTION_KEY)
         return ENCRYPTION_KEY
 
     def __create_key(self, env_path: Path) -> bytes:
         ENCRYPTION_KEY = Fernet.generate_key()
         # save to env with absolute path
+        # because env not inside env -_-
         set_key(str(env_path), 'ENCRYPTION_KEY', ENCRYPTION_KEY.hex())
         return ENCRYPTION_KEY
 
-    """encrypt&decrypt data"""
+    """encrypt&decrypt data methods"""
     def encrypt(self, text: str) -> str:
+        if not text:
+            raise ValueError("Text cannot be empty or None!")
         # str -> bytes -> encrypt -> bytes -> str
         return self.__fernet.encrypt(text.encode()).decode()
 
@@ -47,7 +50,7 @@ class CryptManager:
         # str -> bytes -> decrypt -> bytes -> str
         return self.__fernet.decrypt(text.encode()).decode()
 
-    """password"""
+    """password's methods"""
     def hash_password(self, password: str) -> str:
         return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
 
